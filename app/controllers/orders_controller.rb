3f3db64f5fa15ffb3505815e2f31c63fab31e7cd
Sarah Thiery <sarahvthiery@gmail.com>
2021-06-05 17:21:06 -0400

    Add stripe gem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,6 +19,28 @@ class OrdersController < ApplicationController
         render json: @order
     end
 
+    def create_checkout_session
+        session = Stripe::Checkout::Session.create({
+            payment_method_types: ['card'],
+            line_items: [{
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: 'T-shirt',
+                    },
+                    unit_amount: 2000,
+            },
+            quantity: 1,
+            }],
+            mode: 'payment',
+            # These placeholder URLs will be replaced in a following step.
+            success_url: 'https://example.com/success',
+            cancel_url: 'https://example.com/cancel',
+            })
+
+        { id: session.id }.to_json
+    end
+
     private
 
     def order_params
