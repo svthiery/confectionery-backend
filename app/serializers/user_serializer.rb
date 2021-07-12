4b3c914bd14dec7e60782d2c777f3e22310e69d3
Sarah Thiery <sarahvthiery@gmail.com>
2021-07-12 15:49:49 -0700

    git add . git

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :profile_img, :billing_name, :billing_street1, :billing_street2, :billing_city, :billing_zip, :billing_state, :shipping_name, :shipping_street1, :shipping_street2, :shipping_city, :shipping_zip, :shipping_state, :email
+  attributes :id, :username, :profile_img, :billing_name, :billing_street1, :billing_street2, :billing_city, :billing_zip, :billing_state, :shipping_name, :shipping_street1, :shipping_street2, :shipping_city, :shipping_zip, :shipping_state, :email, :visits
 end
